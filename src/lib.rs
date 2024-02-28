705a8b63376e18997a2e12c86c0ec6cf8da4992f
yamd1 <w5000c2d26@gmail.com>
2024-02-29 08:31:46 +0900

    👔 行数・文字数・単語数・バイト数をカウントして表示できるように

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,10 @@
-use clap::{Arg, ArgAction, ArgMatches, Command};
-use std::error::Error;
+use clap::{Arg, ArgAction, Command};
+
+use std::{
+    error::Error,
+    fs::File,
+    io::{self, BufRead, BufReader},
+};
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
@@ -10,6 +15,14 @@ pub struct Config {
     words: bool,
     bytes: bool,
     chars: bool,
+}
+
+#[derive(Debug, PartialEq)]
+pub struct FileInfo {
+    num_lines: usize,
+    num_words: usize,
+    num_bytes: usize,
+    num_chars: usize,
 }
 
 pub fn get_args() -> MyResult<Config> {
@@ -85,6 +98,58 @@ pub fn get_args() -> MyResult<Config> {
 }
 
 pub fn run(config: Config) -> MyResult<()> {
-    println!("{:?}", config);
+    for filename in config.files {
+        match open(&filename) {
+            Err(err) => eprintln!("{}: {}", filename, err),
+            Ok(buf) => {
+                let fileinfo = count(buf)?;
+                println!(
+                    "{:>8}  {:>8}  {:>8}  {}",
+                    fileinfo.num_lines, fileinfo.num_words, fileinfo.num_bytes, filename
+                );
+            }
+        }
+    }
     Ok(())
+}
+
+fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
+    }
+}
+
+pub fn count(mut file: impl BufRead) -> MyResult<FileInfo> {
+    let mut body = String::new();
+    let mut num_bytes = file.read_line(&mut body)?;
+    let mut num_lines = body.lines().count();
+    let mut num_words = body.split_ascii_whitespace().count();
+    let mut num_chars = body.chars().count();
+
+    Ok(FileInfo {
+        num_lines,
+        num_bytes,
+        num_words,
+        num_chars,
+    })
+}
+#[cfg(test)]
+mod tests {
+    use super::{count, FileInfo};
+    use std::io::Cursor;
+
+    #[test]
+    fn test_count() {
+        let text = "I don't want the world. I just want your half. \r\n";
+        let info = count(Cursor::new(text));
+        assert!(info.is_ok());
+        let expected = FileInfo {
+            num_lines: 1,
+            num_words: 10,
+            num_chars: 48,
+            num_bytes: 48,
+        };
+        assert_eq!(info.unwrap(), expected);
+    }
 }
